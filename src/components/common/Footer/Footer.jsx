aec1afc75fc4e6b2ecf582db51b95ecac07bb9a0
a-colavito <adolfocolavito@hotmail.it>
2024-08-02 18:22:20 +0200

    qualche variazione estetica footer

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="bg-slate-50 border-t  border-black text-dark p-14  dark:border-t dark:border-white dark:bg-blue-950">
+    <div className="bg-slate-50 border-t  border-blue-950 text-dark p-14  dark:border-t dark:border-slate-50 dark:bg-blue-950">
       <div className="flex flex-col items-center space-y-2">
        
     </div>
